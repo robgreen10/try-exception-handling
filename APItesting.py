c78db2e8cfadf006e201277408c5845df4191fb6
Robs Computer <robscomputer@MacBook-Air.local>
2026-05-23 21:06:42 -0400

    fix: remove APItesting.py from source control

--- APItesting.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-response = requests.get("https://randomfox.ca/floof/")
-x = response.json()
-if response.status_code == 200:
-    print(
-        f"Success! You have successfully retrieved the data: {x}")
-
-print(x["link"])
